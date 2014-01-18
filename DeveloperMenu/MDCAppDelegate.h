829ca17d4e56a9df13367dc3068a6b23d382d84d
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-01-18 08:42:12 +0000

    Initial commit

---
+++ DeveloperMenu/MDCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDCAppDelegate.h
+//  DeveloperMenu
+//
+//  Created by Matthew Cheetham on 18/01/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
