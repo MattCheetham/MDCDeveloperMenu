f2b775628a234839831d6e7d06a4c374b44b98be
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-02-02 11:28:23 +0000

    Adds MDCLog object for log support

---
+++ DeveloperMenu/MDCLog.h
@@ -0,0 +1,25 @@
+//
+//  MDCLog.h
+//  DeveloperMenu
+//
+//  Created by Matthew Cheetham on 02/02/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+    MDCLogLevelTrace = 0,
+    MDCLogLevelDebug = 1,
+    MDCLogLevelInfo = 2,
+    MDCLogLevelWarn = 3,
+    MDCLogLevelError = 4,
+    MDCLogLevelFatal = 5
+} MDCLogLevel;
+
+@interface MDCLog : NSObject
+
+@property (nonatomic, assign) MDCLogLevel logLevel;
+@property (nonatomic, strong) NSString *logContent;
+
+@end
---
+++ DeveloperMenu/MDCLog.m
@@ -0,0 +1,13 @@
+//
+//  MDCLog.m
+//  DeveloperMenu
+//
+//  Created by Matthew Cheetham on 02/02/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import "MDCLog.h"
+
+@implementation MDCLog
+
+@end
