693967102eb3b107763bf96a65a055f995c09724
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-02-02 11:16:09 +0000

    Adds shared log controller

---
+++ DeveloperMenu/MDCLogController.h
@@ -0,0 +1,15 @@
+//
+//  MDCLogController.h
+//  DeveloperMenu
+//
+//  Created by Matthew Cheetham on 02/02/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MDCLogController : NSObject
+
++ (MDCLogController *)sharedController;
+
+@end
---
+++ DeveloperMenu/MDCLogController.m
@@ -0,0 +1,34 @@
+//
+//  MDCLogController.m
+//  DeveloperMenu
+//
+//  Created by Matthew Cheetham on 02/02/2014.
+//  Copyright (c) 2014 Matthew Cheetham. All rights reserved.
+//
+
+#import "MDCLogController.h"
+
+@implementation MDCLogController
+
+static MDCLogController *sharedController = nil;
+
++ (MDCLogController *)sharedController
+{
+    @synchronized(self) {
+        if (sharedController == nil) {
+            sharedController = [[self alloc] init];
+        }
+    }
+    return sharedController;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
