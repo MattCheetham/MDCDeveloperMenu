ac8861cd5e375c72e7ce4269eb5d8a92085bf28a
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-02-02 13:26:28 +0000

    Adds definitions of log levels

--- DeveloperMenu/MDCLogController.h
+++ DeveloperMenu/MDCLogController.h
@@ -8,6 +8,32 @@
 
 #import "MDCLog.h"
 
+/*
+MDCLogDebug
+The lowest priority, and normally not logged except for messages from the kernel.
+--
+MDCLogInfo
+The lowest priority that you would normally log, and purely informational in nature.
+--
+MDCLogNotice
+Things of moderate interest to the user or administrator.
+--
+MDCLogWarning
+Something is amiss and might fail if not corrected.
+--
+MDCLogErr
+Something has failed.
+--
+MDCLogCrit
+A failure in a key system.
+--
+MDCLogAlert
+A serious failure in a key system.
+--
+MDCLogEmerg
+The highest priority, usually reserved for catastrophic failures and reboot notices.
+*/
+
 #define MDCLogDebug(string) [[MDCLogController sharedController] addLog:(string) withLevel:MDCLogLevelDebug];
 #define MDCLogInfo(string) [[MDCLogController sharedController] addLog:(string) withLevel:MDCLogLevelInfo];
 #define MDCLogNotice(string) [[MDCLogController sharedController] addLog:(string) withLevel:MDCLogLevelNotice];
