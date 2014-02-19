6e0128bc8923a6e8e500b80efcf737d8022f667c
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-02-19 23:00:01 +0000

    Adds more accurate device name descriptions

--- DeveloperMenu/MDCDeviceInformationController.m
+++ DeveloperMenu/MDCDeviceInformationController.m
@@ -11,6 +11,7 @@
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
 #import <CoreTelephony/CTCarrier.h>
 #import <SystemConfiguration/CaptiveNetwork.h>
+#import <sys/utsname.h>
 
 @interface MDCDeviceInformationController ()
 
@@ -68,7 +69,7 @@ static MDCDeviceInformationController *sharedController = nil;
     
     //General
     MDCDeviceInformationItem *deviceName = [MDCDeviceInformationItem itemWithProperty:@"Name" value:self.currentDevice.name category:@"General"];
-    MDCDeviceInformationItem *deviceModel = [MDCDeviceInformationItem itemWithProperty:@"Model" value:self.currentDevice.model category:@"General"];
+    MDCDeviceInformationItem *deviceModel = [MDCDeviceInformationItem itemWithProperty:@"Model" value:[self localisedDeviceModel] category:@"General"];
     MDCDeviceInformationItem *deviceSystemVersion = [MDCDeviceInformationItem itemWithProperty:@"System Version" value:self.currentDevice.systemVersion category:@"General"];
     MDCDeviceInformationItem *deviceMultiTaskingSupport = [MDCDeviceInformationItem itemWithProperty:@"Multitasking Supported" value:self.currentDevice.multitaskingSupported ? @"Yes" : @"No" category:@"General"];
     MDCDeviceInformationItem *deviceIdentifierForVendor = [MDCDeviceInformationItem itemWithProperty:@"Identifier For Vendor" value:self.currentDevice.identifierForVendor.UUIDString category:@"General"];
@@ -165,6 +166,57 @@ static MDCDeviceInformationController *sharedController = nil;
     }
     
     return deviceItems;
+}
+
+- (NSString *)localisedDeviceModel
+{
+    //Get system device name
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    NSString *deviceCodeName = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+    
+    //Setup dictionary of names
+    NSDictionary *deviceNamesByCode = @{@"i386"      :@"Simulator",
+                          @"iPod1,1"   :@"iPod Touch (Original)",
+                          @"iPod2,1"   :@"iPod Touch (Second Generation)",
+                          @"iPod3,1"   :@"iPod Touch (Third Generation)",
+                          @"iPod4,1"   :@"iPod Touch (Fourth Generation)",
+                          @"iPod5,1"   :@"iPod Touch (Fifth Generation)",
+                          @"iPhone1,1" :@"iPhone (Original)",
+                          @"iPhone1,2" :@"iPhone 3G",
+                          @"iPhone2,1" :@"iPhone 3GS",
+                          @"iPhone3,1" :@"iPhone 4",
+                          @"iPhone3,2" :@"iPhone 4",
+                          @"iPhone3,3" :@"iPhone 4",
+                          @"iPhone4,1" :@"iPhone 4S",
+                          @"iPhone5,1" :@"iPhone 5 (AT&T/Canada)",
+                          @"iPhone5,2" :@"iPhone 5",
+                          @"iPhone5,3" :@"iPhone 5C",
+                          @"iPhone5,4" :@"iPhone 5C",
+                          @"iPhone6,1" :@"iPhone 5S",
+                          @"iPhone6,2" :@"iPhone 5S",
+                          @"iPad1,1"   :@"iPad",
+                          @"iPad2,1"   :@"iPad 2",
+                          @"iPad2,2"   :@"iPad 2",
+                          @"iPad2,3"   :@"iPad 2",
+                          @"iPad2,4"   :@"iPad 2",
+                          @"iPad3,1"   :@"iPad (3rd Generation)",
+                          @"iPad3,2"   :@"iPad (3rd Generation)",
+                          @"iPad3,3"   :@"iPad (3rd Generation)",
+                          @"iPad3,4"   :@"iPad (4th Generation)",
+                          @"iPad3,5"   :@"iPad (4th Generation)",
+                          @"iPad3,6"   :@"iPad (4th Generation)",
+                          @"iPad2,5"   :@"iPad Mini",
+                          @"iPad4,1"   :@"iPad Air (Wifi)",
+                          @"iPad4,2"   :@"iPad Air (Cellular)",
+                          @"iPad2,5"   :@"iPad Mini",
+                          @"iPad2,6"   :@"iPad Mini",
+                          @"iPad2,7"   :@"iPad Mini",
+                          @"iPad4,4"   :@"iPad Mini Retina (Wifi)",
+                          @"iPad4,5"   :@"iPad Mini Retina (Cellular)"
+                          };
+    
+    return deviceNamesByCode[deviceCodeName] ? deviceNamesByCode[deviceCodeName] : deviceCodeName;
 }
 
 @end
