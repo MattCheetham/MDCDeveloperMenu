582fb2188b91a4169e5b6e2866e9a29d9d434634
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-02-20 20:15:48 +0000

    Removes boilerplate code for UITableViewCells

--- DeveloperMenu/MDCCell.m
+++ DeveloperMenu/MDCCell.m
@@ -14,16 +14,9 @@
 {
     self = [super initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseIdentifier];
     if (self) {
-        // Initialization code
+        
     }
     return self;
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
