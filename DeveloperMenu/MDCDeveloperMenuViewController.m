f1680f05d6127c3ac6ea91397d781b2996113db5
Matthew Cheetham <m4ttcheetham@gmail.com>
2014-02-21 06:56:20 +0000

    Adds access to "Send report" via main dev menu

--- DeveloperMenu/MDCDeveloperMenuViewController.m
+++ DeveloperMenu/MDCDeveloperMenuViewController.m
@@ -13,10 +13,12 @@
 #import "MDCUserDefaultsController.h"
 #import "MDCLogBrowserViewController.h"
 #import "MDCDeviceInformationBrowserViewController.h"
+#import "MDCLogController.h"
 
 @interface MDCDeveloperMenuViewController ()
 
 @property (nonatomic, strong) UIViewController *attatchedViewController;
+@property (nonatomic, strong) MDCLogController *logController;
 
 @end
 
@@ -30,6 +32,8 @@
         self.title = @"Developer Menu"; 
         
         [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+        
+        self.logController = [MDCLogController sharedController];
         
     }
     return self;
@@ -69,12 +73,23 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 1;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 4;
+    switch (section) {
+        case 0:
+            return 4;
+            break;
+        case 1:
+            return 1;
+            break;
+            
+        default:
+            return 0;
+            break;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -86,14 +101,26 @@
 
     // Configure the cell...
     
-    if(indexPath.row == 0){
-        cell.textLabel.text = @"View Info Plist";
-    } else if(indexPath.row == 1){
-        cell.textLabel.text = @"View NSUserDefaults";
-    } else if(indexPath.row == 2){
-        cell.textLabel.text = @"View console logs";
-    } else if(indexPath.row == 3){
-        cell.textLabel.text = @"View device information";
+    if(indexPath.section == 0){
+        
+        if(indexPath.row == 0){
+            cell.textLabel.text = @"View Info Plist";
+        } else if(indexPath.row == 1){
+            cell.textLabel.text = @"View NSUserDefaults";
+        } else if(indexPath.row == 2){
+            cell.textLabel.text = @"View console logs";
+        } else if(indexPath.row == 3){
+            cell.textLabel.text = @"View device information";
+        }
+        
+    } else if(indexPath.section == 1){
+        
+        if(indexPath.row == 0){
+            
+            cell.textLabel.text = @"Send device report";
+            
+        }
+        
     }
     
     return cell;
@@ -118,29 +145,53 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
-    if(indexPath.row == 0){
+    if(indexPath.section == 0){
+        
+        if(indexPath.row == 0){
 
-        MDCPlistBrowserViewController *plistBrowser = [[MDCPlistBrowserViewController alloc] initWithPlistItems:[MDCPlistController sharedController].plistItems];
+            MDCPlistBrowserViewController *plistBrowser = [[MDCPlistBrowserViewController alloc] initWithPlistItems:[MDCPlistController sharedController].plistItems];
 
-        [self.navigationController pushViewController:plistBrowser animated:YES];
-        
-    } else if(indexPath.row == 1){
+            [self.navigationController pushViewController:plistBrowser animated:YES];
+            
+        } else if(indexPath.row == 1){
 
-        MDCUserDefaultsBrowserViewController *defaultsBrowser = [[MDCUserDefaultsBrowserViewController alloc] initWithUserDefaultsItems:[MDCUserDefaultsController sharedController].userDefaultsItems];
+            MDCUserDefaultsBrowserViewController *defaultsBrowser = [[MDCUserDefaultsBrowserViewController alloc] initWithUserDefaultsItems:[MDCUserDefaultsController sharedController].userDefaultsItems];
+            
+            [self.navigationController pushViewController:defaultsBrowser animated:YES];
+            
+        } else if(indexPath.row == 2){
+            
+            MDCLogBrowserViewController *logBrowser = [MDCLogBrowserViewController new];
+            
+            [self.navigationController pushViewController:logBrowser animated:YES];
+            
+        } else if(indexPath.row == 3){
+            
+            MDCDeviceInformationBrowserViewController *deviceInfoBrowser = [MDCDeviceInformationBrowserViewController new];
+            
+            [self.navigationController pushViewController:deviceInfoBrowser animated:YES];
+            
+        }
         
-        [self.navigationController pushViewController:defaultsBrowser animated:YES];
+    } else if(indexPath.section == 1){
         
-    } else if(indexPath.row == 2){
-        
-        MDCLogBrowserViewController *logBrowser = [MDCLogBrowserViewController new];
-        
-        [self.navigationController pushViewController:logBrowser animated:YES];
-        
-    } else if(indexPath.row == 3){
-        
-        MDCDeviceInformationBrowserViewController *deviceInfoBrowser = [MDCDeviceInformationBrowserViewController new];
-        
-        [self.navigationController pushViewController:deviceInfoBrowser animated:YES];
+        if(indexPath.row == 0){
+            
+            [self.logController generateLogFileWithcompletion:^(NSURL *filePath, NSError *error) {
+                
+                if(!error){
+                    
+                    MDCLogInfo(@"File can be found at:%@", [filePath path]);
+                    
+                } else {
+                    
+                    MDCLogErr(@"%@", error);
+                    
+                }
+                
+            }];
+            
+        }
         
     }
 }
